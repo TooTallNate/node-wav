7856d7fae044104d2e5642ab8dfadaf6af91ac78
Nathan Rajlich <nathan@tootallnate.net>
2012-10-16 16:55:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+/**
+ * The `Reader` class accepts a WAVE audio file, emits a "format" event, and
+ * outputs the raw "data" from the WAVE file (usually raw PCM data, but if the
+ * WAVE file uses compression then the compressed data will be output, you are
+ * responsible for uncompressing in that case if necessary).
+ */
+
+exports.Reader = require('./lib/reader');
+
+/**
+ * The `Writer` class outputs a valid WAVE file from the audio data written to
+ * it. You may set any of the "channels", "sampleRate" or "bitsPerSample"
+ * properties before writing the first chunk. You may also set the "dataLength" to
+ * the number of bytes expected in the "data" portion of the WAVE file. If
+ * "dataLength" is not set, then the maximum valid length for a WAVE file is
+ * written.
+ */
+
+exports.Writer = require('./lib/writer');
