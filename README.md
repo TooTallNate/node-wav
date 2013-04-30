e5405bf76c74d861e7c20db2fb5f84b7e3588ce4
Chris Roberts <chris@naxxfish.eu>
2013-04-30 18:05:20 +0100

    # This is a combination of 13 commits. # The first commit's message is: test: cart chunk support (sample from http://www.cartchunk.org/)
    Signed-off-by: Chris Roberts <chris@naxxfish.eu>
    
    # This is the 2nd commit message:
    
    Test commit!
    
    Signed-off-by: Chris Roberts <chris@naxxfish.eu>
    
    # This is the 3rd commit message:
    
    Refactored subchunk handling to allow for more than just "fmt " and "data" - also added "unknown" event emission for unknown chunks
    
    Signed-off-by: Chris Roberts <chris@naxxfish.eu>
    
    # This is the 4th commit message:
    
    Added support for EBU BWF files ("bext" chunk)
    
    Signed-off-by: Chris Roberts <chris@naxxfish.eu>
    
    # This is the 5th commit message:
    
    Added cart chunk support and tests
    
    Signed-off-by: Chris Roberts <chris@naxxfish.eu>
    
    # This is the 6th commit message:
    
    Added documentation on experimental cart and bext support
    
    Signed-off-by: Chris Roberts <chris@naxxfish.eu>
    
    # This is the 7th commit message:
    
    Deleted CC_0101.WAV (wrong case in the extension).  Travis WILL fail for this commit
    
    Signed-off-by: Chris Roberts <chris@naxxfish.eu>
    
    # This is the 8th commit message:
    
    Changed the extension to lower case.  Should fix npm test.
    
    Signed-off-by: Chris Roberts <chris@naxxfish.eu>
    
    # This is the 9th commit message:
    
    Data chunks are now parsed rather than passthru'd
    
    # This is the 10th commit message:
    
    Fixed test
    
    # This is the 11th commit message:
    
    Added dev-null, fixed wavinfo so it works
    
    # This is the 12th commit message:
    
    wavinfo now pipes to dev-null rather than a temporary file
    
    # This is the 13th commit message:
    
    Added support for a few different formats (other than PCM)

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ node-wav
 [![Build Status](https://secure.travis-ci.org/TooTallNate/node-wav.svg)](http://travis-ci.org/TooTallNate/node-wav)
 
 This module offers streams to help work with Microsoft WAVE files.
-
+Test
 
 Installation
 ------------
@@ -58,6 +58,14 @@ be output, depending on the `audioFormat` property).
 A `"format"` event gets emitted after the WAV header has been parsed.
 
 ### Writer()
+## EXPERIMENTAL
+A `"cart"` event is emitted if a ["CartChunk"](http://www.cartchunk.org/) subchunk is detected
+
+A `"bext"` event is emitted if a ["Broadcast Wave File"](http://en.wikipedia.org/wiki/Broadcast_Wave_Format) 
+subchunk is detected
+
+__NOTE__: At the moment, this will only be for subchunks __before__ the `"data"` chunk. This isn't always the case - some applications
+will write these chunks at the end of the file! 
 
 The `Writer` class accepts raw audio data written to it (only PCM audio data is
 currently supported), and outputs a WAV file with a valid WAVE header at the
--- test/reader.js
+++ test/reader.js
@@ -117,7 +117,54 @@ describe('Reader', function () {
 
 
   });
+  describe('BWF - Broadcast Wave Extension', function () {
+  	describe('CC_0101.wav', function () {
+		var fixture = path.resolve(__dirname, 'fixtures', 'CC_0101.wav');
+		it('should emit a "bext" event', function (done) {
+			var reader = new Reader();
+			reader.on('bext', function (bext) {
+				assert.equal('Dick Pierce', bext.Originator);
+				assert.equal('CartChunk.org', bext.OriginatorReference);
+				assert.equal('2001/09/13', bext.OriginationDate);
+				assert.equal('07:50:05', bext.OriginationTime); // Dick gets up early :)
+				assert.equal('CartChunk.org linear PCM sample file containing \'bext\' and \'cart\' chunks', 
+					bext.Description);
+				assert.equal(0,bext.TimeReferenceLow);
+				assert.equal(0,bext.TimeReferenceHigh);
+				done();
+			});
+			fs.createReadStream(fixture).pipe(reader).resume();
+		});
+		it('should emit a "cart" event', function (done) {
+			var reader = new Reader();
+			reader.on('cart', function (cart) {
+				assert.equal('Cart Chunk: the traffic data file format for the Radio Industry', cart.title);
+				assert.equal('Jay Rose, dplay.com', cart.artist);
+				assert.equal('DEMO-0101', cart.cut_id);
+				assert.equal('CartChunk.org', cart.client_id);
+				assert.equal('DEMO', cart.category);
+				assert.equal('1900/01/01', cart.start_date);
+				assert.equal('00:00:00',cart.start_time);
+				assert.equal('2099/12/31',cart.end_date);
+				assert.equal('23:59:59',cart.end_time);
+				assert.equal('AUDICY',cart.producer_app_id);
+				assert.equal('3.10/623',cart.producer_app_version);
+				assert.equal('Demo ID showing basic \'cart\' chunk attributes', cart.userdef);
+				assert.equal(32768, cart.level_reference);
+				assert.equal('the Radio Industry', cart.outcue);
+				assert.equal('EOD',cart.timers[0].usage);
+				assert.equal(201024, cart.timers[0].value);
+				assert.equal('SEC1', cart.timers[6].usage);
+				assert.equal(152533, cart.timers[6].value);
+				assert.equal('MRK', cart.timers[7].usage);
+				assert.equal(112000, cart.timers[7].value);
+				done();
+			});
+			fs.createReadStream(fixture).pipe(reader).resume();
+		});
+	});
 
+  });
   describe('RIFX - Big-endian', function () {
 
     describe('gameover-rifx.wav', function () {
