6f578b667889b699cd9d0e5d5ca77c13a85b52b0
Nathan Rajlich <nathan@tootallnate.net>
2014-06-21 13:38:33 -0700

    reader: add initial `float` WAV file support
    Kinda sloppy at the moment. I need to extract this out into
    a proper RIFF format parser which node-wav can be implemented
    on top of.

--- test/reader.js
+++ test/reader.js
@@ -65,6 +65,60 @@ describe('Reader', function () {
 
     });
 
+    describe('M1F1-float32-AFsp.wav', function () {
+      var fixture = path.resolve(__dirname, 'fixtures', 'M1F1-float32-AFsp.wav');
+
+      it('should emit a "format" event', function (done) {
+        var reader = new Reader();
+        reader.on('format', function (format) {
+          assert.equal(3, format.audioFormat);
+          assert.equal(2, format.channels);
+          assert.equal(8000, format.sampleRate);
+          assert.equal(32, format.bitDepth);
+          assert.equal(true, format.signed);
+          assert.equal(true, format.float);
+          done();
+        });
+        reader.on('chunk', function (chunk) {
+          console.log(chunk);
+        });
+        fs.createReadStream(fixture).pipe(reader).resume();
+      });
+
+      it('should emit an "end" event', function (done) {
+        var reader = new Reader();
+        reader.on('end', done);
+        fs.createReadStream(fixture).pipe(reader).resume();
+      });
+
+    });
+
+    describe('M1F1-float64-AFsp.wav', function () {
+      var fixture = path.resolve(__dirname, 'fixtures', 'M1F1-float64-AFsp.wav');
+
+      it('should emit a "format" event', function (done) {
+        var reader = new Reader();
+        reader.on('format', function (format) {
+          assert.equal(3, format.audioFormat);
+          assert.equal(2, format.channels);
+          assert.equal(8000, format.sampleRate);
+          assert.equal(64, format.bitDepth);
+          assert.equal(true, format.signed);
+          assert.equal(true, format.float);
+          done();
+        });
+        fs.createReadStream(fixture).pipe(reader).resume();
+      });
+
+      it('should emit an "end" event', function (done) {
+        var reader = new Reader();
+        reader.on('end', done);
+        fs.createReadStream(fixture).pipe(reader).resume();
+      });
+
+    });
+
+
   });
 
   describe('RIFX - Big-endian', function () {
